d7c25ac448c55fdedf88913f7678158a4860f40c
hoangtuan123 <nghoangtuan92@gmail.com>
2016-04-25 11:58:07 +0700

    update20160425
    Config

--- protected/config/database.php
+++ protected/config/database.php
@@ -4,19 +4,13 @@
 return array(
 	//'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
-	/*
+	
 	'connectionString' => 'mysql:host=localhost;dbname=databasecancau',
 	'emulatePrepare' => true,
 	'username' => 'root',
 	'password' => '',
 	'charset' => 'utf8',
-	*/
-
-	'connectionString' => 'mysql:host=localhost;dbname=',
-	'emulatePrepare' => true,
-	'username' => 'root',
-	'password' => '',
-	'charset' => 'utf8',
 	
+
 	
 );
